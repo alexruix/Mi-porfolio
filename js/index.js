614dd054dad6d8fe204ee4b0c29b50e3ab1e5235
LVL20 <93489899+LVL20@users.noreply.github.com>
2021-12-15 01:05:27 -0300

    New Version

--- js/index.js
+++ js/index.js
@@ -1,21 +1,4 @@
-$(document).ready(function(){
-    $('#type').typewrite({
-        actions: [
-            {type: 'Hello. '},
-            {type: '<br>'},
-            {type: 'Weclome '},
-            {delay: 1500},
-            {remove: {num: 1, type: 'stepped'}},
-            {select: {from: 11, to: 16}},
-            {delay: 2000},
-            {remove: {num: 5, type: 'whole'}},
-            {delay: 300},
-            {type: 'lcome to typewrite. '},
-            {type: '<br>'},
-            {type: 'It\'s just so easy to setup and use.'}
-        ]
-    });
-});
+
 
 const buttons = document.querySelectorAll(".menu__item");
 let activeButton = document.querySelector(".menu__item.active");
