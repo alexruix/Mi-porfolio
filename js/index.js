93dbaa0c6a3d4c50787cdcf393596f22aba687d5
alexruix <ruizgelp@gmail.com>
2021-12-15 03:03:24 -0300

    New Version

--- js/index.js
+++ js/index.js
@@ -48,3 +48,18 @@ function handleTransition(item, text) {
     });
 
 }
+var app = document.getElementById('type');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();
