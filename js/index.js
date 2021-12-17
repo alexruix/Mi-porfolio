c1f00086c793d5a2870e483a1b3d3fbd5c9565cd
alexruix <ruizgelp@gmail.com>
2021-12-17 03:09:06 -0300

    New Version

--- js/index.js
+++ js/index.js
@@ -32,7 +32,6 @@ typewriter.typeString('modernas')
 
 feather.replace()
 
-document.querySelectorAll('.btn').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
 
 const buttons = document.querySelectorAll(".menu__item");
 let activeButton = document.querySelector(".menu__item.active");
