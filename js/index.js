41b53a72bdc0039c7abbacc4a79985d9c8527fd2
LVL20 <93489899+LVL20@users.noreply.github.com>
2022-01-29 19:11:07 -0300

    New Version

--- js/index.js
+++ js/index.js
@@ -21,23 +21,21 @@ typewriter.typeString('modernas')
 
 feather.replace()
 
-// var messageBox = document.querySelector('.js-message');
-//   var btn = document.querySelector('.js-message-btn');
-//   var card = document.querySelector('.js-profile-card');
-//   var closeBtn = document.querySelectorAll('.js-message-close');
-//
-//   btn.addEventListener('click',function (e) {
-//       e.preventDefault();
-//       card.classList.add('active');
-//   });
-//
-//   closeBtn.forEach(function (element, index) {
-//      console.log(element);
-//       element.addEventListener('click',function (e) {
-//           e.preventDefault();
-//           card.classList.remove('active');
-//       });
-//   });
+$(function() {
+  let social = $(".social");
+
+  // if(document.body.classList.contains("dark")){
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll <= 2750) {
+      social.show();
+    } else {
+      social.hide();
+    }
+  });
+});
+
 
   $(document).ready(function(){
       $('.modal').modal();
